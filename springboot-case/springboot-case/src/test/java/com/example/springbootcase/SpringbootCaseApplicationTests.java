59d432e30269d1a1321aa91c70c537dc789224af
KaslanaNiet <130820243+KaslanaNiet@users.noreply.github.com>
2024-08-27 19:00:38 +0800

    Delete springboot-case/springboot-case directory

--- springboot-case/springboot-case/src/test/java/com/example/springbootcase/SpringbootCaseApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbootcase;
-
-import com.niet.BigEventApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = BigEventApplication.class)
-public class SpringbootCaseApplicationTests {
-    // 测试代码
-}
